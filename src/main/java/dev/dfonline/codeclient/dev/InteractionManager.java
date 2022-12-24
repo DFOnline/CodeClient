8d4092b3b6125d203e6e0eeedaf55a42f2dec7b3
GeorgeRNG <grogrng@gmail.com>
2022-12-24 11:42:33 +0000

    Readd the sounds

--- src/main/java/dev/dfonline/codeclient/dev/InteractionManager.java
+++ src/main/java/dev/dfonline/codeclient/dev/InteractionManager.java
@@ -2,13 +2,18 @@ package dev.dfonline.codeclient.dev;
 
 import dev.dfonline.codeclient.CodeClient;
 import dev.dfonline.codeclient.PlotLocation;
+import net.minecraft.block.*;
+import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 
 import java.util.List;
 
@@ -39,7 +44,7 @@ public class InteractionManager {
     public static boolean onPlaceBlock(BlockPos pos) {
         if(isInCodeSpace(pos)) {
             CodeClient.MC.player.swingHand(Hand.MAIN_HAND);
-//            CodeClient.MC.player.playSound(new SoundEvent());
+            CodeClient.MC.getSoundManager().play(new PositionedSoundInstance(new SoundEvent(new Identifier("minecraft:block.stone.place")), SoundCategory.BLOCKS, 2, 0.8F, Random.create(), pos));
             return true;
         }
         return false;
@@ -47,9 +52,10 @@ public class InteractionManager {
 
     private static void breakCodeBlock(BlockPos pos) {
         ClientWorld world = CodeClient.MC.world;
-        world.breakBlock(pos, false);
-        world.breakBlock(pos.add(0,1,0), false);
-        world.breakBlock(pos.add(-1,0,0), false);
-        world.breakBlock(pos.add(0,0,1), false);
+        CodeClient.MC.getSoundManager().play(new PositionedSoundInstance(new SoundEvent(new Identifier("minecraft:block.stone.break")), SoundCategory.BLOCKS, 2, 0.8F, Random.create(), pos));
+        world.setBlockState(pos, Blocks.AIR.getDefaultState());
+        world.setBlockState(pos.add(0,1,0), Blocks.AIR.getDefaultState());
+        world.setBlockState(pos.add(-1,0,0), Blocks.AIR.getDefaultState());
+        world.setBlockState(pos.add(0,0,1), Blocks.AIR.getDefaultState());
     }
 }
