adf6cf39d3edb46f70bd70d00af3d46cdf53e4b3
GeorgeRNG <grogrng@gmail.com>
2023-03-04 18:54:09 +0000

    A little bit of lore

--- src/main/java/dev/dfonline/codeclient/actiondump/Item.java
+++ src/main/java/dev/dfonline/codeclient/actiondump/Item.java
@@ -3,6 +3,10 @@ package dev.dfonline.codeclient.actiondump;
 import dev.dfonline.codeclient.CodeClient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtInt;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtString;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -24,7 +28,20 @@ public class Item {
 
     public ItemStack getItem() {
         ItemStack item = Registry.ITEM.get(new Identifier(material.toLowerCase())).getDefaultStack();
+
+        NbtCompound nbt = new NbtCompound();
+        NbtCompound display = new NbtCompound();
+        NbtList lore = new NbtList();
+        for (String line: description) {
+            lore.add(NbtString.of("{\"extra\":[{\"bold\":false,\"italic\":false,\"underlined\":false,\"strikethrough\":false,\"obfuscated\":false,\"color\":\"gray\",\"text\":\"%s\"}],\"text\":\"\"}".formatted(line)));
+        }
+        display.put("Lore",lore);
+        nbt.put("display",display);
+        nbt.put("HideFlags", NbtInt.of(127));
+        item.setNbt(nbt);
+
         item.setCustomName(Text.literal("§b" + name));
+
         return item;
     }
 }
