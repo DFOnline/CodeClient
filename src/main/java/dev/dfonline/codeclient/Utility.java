3d7b49a80d21642b3bf04db00757bbb362ee2234
GeorgeRNG <grogrng@gmail.com>
2023-03-07 19:07:40 +0000

    untested moment

--- src/main/java/dev/dfonline/codeclient/Utility.java
+++ src/main/java/dev/dfonline/codeclient/Utility.java
@@ -1,11 +1,16 @@
 package dev.dfonline.codeclient;
 
+import com.google.gson.JsonObject;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.zip.GZIPOutputStream;
 
 public class Utility {
     public static void makeHolding(ItemStack template) {
@@ -28,5 +33,17 @@ public class Utility {
             templates.add(item);
         }
         return templates;
+    }
+
+    public static String compileTemplate(JsonObject data) throws IOException {
+        return compileTempate(data.getAsString());
+    }
+    public static String compileTempate(String data) throws IOException {
+        ByteArrayOutputStream obj = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(obj);
+        gzip.write(data.getBytes());
+        gzip.close();
+
+        return Base64.getEncoder().encode(obj.toByteArray()).toString();
     }
 }
--- src/main/java/dev/dfonline/codeclient/actiondump/Action.java
+++ src/main/java/dev/dfonline/codeclient/actiondump/Action.java
@@ -1,5 +1,13 @@
 package dev.dfonline.codeclient.actiondump;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import dev.dfonline.codeclient.CodeClient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtString;
+
 public class Action {
     public String name;
     public String codeblockName;
@@ -15,5 +23,27 @@ public class Action {
         }
         catch (Exception ignored) {}
         return null;
+    }
+
+    public ItemStack getItem() {
+        ItemStack item = icon.getItem();
+        NbtCompound nbt = item.getNbt();
+        assert nbt != null;
+        NbtCompound PublicBukkitValues = new NbtCompound();
+        JsonObject codetemplatedata = new JsonObject();
+        codetemplatedata.addProperty("author", CodeClient.MC.getSession().getUsername());
+        codetemplatedata.addProperty("name", icon.name);
+        codetemplatedata.addProperty("version", 1);
+
+//        JsonObject template = new JsonObject();
+//        JsonArray blocks = new JsonArray();
+//        JsonObject action = new JsonObject();
+//        action.addProperty("");
+//        template.
+
+        PublicBukkitValues.put("hypercube:codetemplatedata", NbtString.of(codetemplatedata.getAsString()));
+        nbt.put("PublicBukkitValues",PublicBukkitValues);
+        item.setNbt(nbt);
+        return item;
     }
 }
