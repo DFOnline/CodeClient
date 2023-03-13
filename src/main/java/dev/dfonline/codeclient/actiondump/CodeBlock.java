dd175e8a49cea914ecef33a64b054599128aa2b6
GeorgeRNG <grogrng@gmail.com>
2023-03-13 20:19:40 +0000

    Small changes

--- src/main/java/dev/dfonline/codeclient/actiondump/CodeBlock.java
+++ src/main/java/dev/dfonline/codeclient/actiondump/CodeBlock.java
@@ -1,7 +1,21 @@
 package dev.dfonline.codeclient.actiondump;
 
-public class CodeBlock {
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
+public class CodeBlock implements Searchable {
     public String name;
     public String identifier;
     public Icon item;
+
+    @Override
+    public List<String> getTerms() {
+        return List.of(name, item.getName());
+    }
+
+    @Override
+    public ItemStack getItem() {
+        return item.getItem();
+    }
 }
