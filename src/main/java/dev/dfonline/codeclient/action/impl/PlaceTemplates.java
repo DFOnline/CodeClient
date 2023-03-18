7ab09bf4886252762f9d4fabc7bf0b58d6da9505
GeorgeRNG <grogrng@gmail.com>
2023-03-18 11:46:10 +0000

    Fix #7

--- src/main/java/dev/dfonline/codeclient/action/impl/PlaceTemplates.java
+++ src/main/java/dev/dfonline/codeclient/action/impl/PlaceTemplates.java
@@ -5,6 +5,7 @@ import dev.dfonline.codeclient.MoveToLocation;
 import dev.dfonline.codeclient.Utility;
 import dev.dfonline.codeclient.action.Action;
 import dev.dfonline.codeclient.location.Dev;
+import dev.dfonline.codeclient.location.Plot;
 import net.minecraft.entity.player.PlayerInventory;
 import dev.dfonline.codeclient.mixin.ClientPlayerInteractionManagerAccessor;
 import net.minecraft.item.ItemStack;
@@ -38,32 +39,34 @@ public class PlaceTemplates extends Action {
     }
 
     static void placeTemplateAt(int row, int level) {
-        Dev plot = (Dev) CodeClient.location;
-        Vec3d pos = new Vec3d(plot.getX(), 50, plot.getZ()).add((2 + (row * 3)) * -1, level * 5, 0);
-        new MoveToLocation(CodeClient.MC.player).setPos(pos.add(1,2,1));
-        BlockHitResult blockHitResult = new BlockHitResult(pos.add(0,1,0), Direction.UP, new BlockPos.Mutable(pos.x, pos.y , pos.z), false);
-        ((ClientPlayerInteractionManagerAccessor) (CodeClient.MC.interactionManager)).invokeSequencedPacket(CodeClient.MC.world, sequence -> new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND, blockHitResult, sequence));
-//        CodeClient.MC.interactionManager.interactBlock(CodeClient.MC.player, Hand.MAIN_HAND, blockHitResult);
+        if(CodeClient.location instanceof Dev plot) {
+            Vec3d pos = new Vec3d(plot.getX(), 50, plot.getZ()).add((2 + (row * 3)) * -1, level * 5, 0);
+            new MoveToLocation(CodeClient.MC.player).setPos(pos.add(1,2,1));
+            BlockHitResult blockHitResult = new BlockHitResult(pos.add(0,1,0), Direction.UP, new BlockPos.Mutable(pos.x, pos.y , pos.z), false);
+            ((ClientPlayerInteractionManagerAccessor) (CodeClient.MC.interactionManager)).invokeSequencedPacket(CodeClient.MC.world, sequence -> new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND, blockHitResult, sequence));
+//            CodeClient.MC.interactionManager.interactBlock(CodeClient.MC.player, Hand.MAIN_HAND, blockHitResult);
+        }
     }
 
     @Override
     public void onTick() {
-        Dev plot = (Dev) CodeClient.location;
-        if(currentIndex >= templates.size()) {
-            currentIndex = -1;
-            Utility.makeHolding(recoverMainHand);
-            this.callback();
-        };
-        if(currentIndex == -1) return;
-        timeSinceLastTick = timeSinceLastTick + 1;
-        if(timeSinceLastTick == 5) {
-            timeSinceLastTick = 0;
-            Utility.makeHolding(templates.get(currentIndex));
-            int level = currentIndex / rowSize;
-            placeTemplateAt(currentIndex % rowSize, level);
-            currentIndex += 1;
-            if(currentIndex % rowSize == 0) {
-                MoveToLocation.shove(CodeClient.MC.player, new Vec3d(plot.getX(), 50, plot.getZ()).add(0,(level * 5) + 2,0));
+        if(CodeClient.location instanceof Dev plot) {
+            if(currentIndex >= templates.size()) {
+                currentIndex = -1;
+                Utility.makeHolding(recoverMainHand);
+                this.callback();
+            };
+            if(currentIndex == -1) return;
+            timeSinceLastTick = timeSinceLastTick + 1;
+            if(timeSinceLastTick == 5) {
+                timeSinceLastTick = 0;
+                Utility.makeHolding(templates.get(currentIndex));
+                int level = currentIndex / rowSize;
+                placeTemplateAt(currentIndex % rowSize, level);
+                currentIndex += 1;
+                if(currentIndex % rowSize == 0) {
+                    MoveToLocation.shove(CodeClient.MC.player, new Vec3d(plot.getX(), 50, plot.getZ()).add(0,(level * 5) + 2,0));
+                }
             }
         }
     }
