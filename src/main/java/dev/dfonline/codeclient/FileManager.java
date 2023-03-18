a4f4bace53619fea7912d5876bb4266c1e6d0464
GeorgeRNG <grogrng@gmail.com>
2023-03-18 12:33:18 +0000

    Fix #6

--- src/main/java/dev/dfonline/codeclient/FileManager.java
+++ src/main/java/dev/dfonline/codeclient/FileManager.java
@@ -22,6 +22,6 @@ public class FileManager {
     }
 
     public static String readFile(String fileName) throws IOException {
-        return Files.readString(Path().resolve(fileName), StandardCharsets.UTF_8);
+        return Files.readString(Path().resolve(fileName), StandardCharsets.ISO_8859_1);
     }
 }
