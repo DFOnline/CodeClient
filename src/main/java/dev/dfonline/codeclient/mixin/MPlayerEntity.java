90b2d7801c386e261954243c8ff2ddb4fe9655fe
GeorgeRNG <grogrng@gmail.com>
2023-04-15 08:47:06 +0000

    Fix #13

--- src/main/java/dev/dfonline/codeclient/mixin/MPlayerEntity.java
+++ src/main/java/dev/dfonline/codeclient/mixin/MPlayerEntity.java
@@ -3,13 +3,10 @@ package dev.dfonline.codeclient.mixin;
 import dev.dfonline.codeclient.CodeClient;
 import dev.dfonline.codeclient.config.Config;
 import dev.dfonline.codeclient.dev.NoClip;
-import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
@@ -17,7 +14,7 @@ public abstract class MPlayerEntity {
 
     @Inject(method = "getOffGroundSpeed", at = @At("HEAD"), cancellable = true)
     private void getAirSpeed(CallbackInfoReturnable<Float> cir) {
-        if(NoClip.ignoresWalls()) {
+        if(NoClip.ignoresWalls() && !CodeClient.MC.player.getAbilities().flying) {
             cir.setReturnValue(0.026F * (CodeClient.MC.player.getMovementSpeed() * Config.getConfig().AirSpeed));
         }
     }
