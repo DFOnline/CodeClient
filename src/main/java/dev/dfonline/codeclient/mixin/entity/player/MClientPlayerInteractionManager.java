27ab281d2278859b29cd395c97d18399b0894694
GeorgeRNG <grogrng@gmail.com>
2023-05-25 18:52:00 +0100

    Stop bad position update when using item: fix #23

--- src/main/java/dev/dfonline/codeclient/mixin/entity/player/MClientPlayerInteractionManager.java
+++ src/main/java/dev/dfonline/codeclient/mixin/entity/player/MClientPlayerInteractionManager.java
@@ -3,20 +3,21 @@ package dev.dfonline.codeclient.mixin.entity.player;
 import dev.dfonline.codeclient.CodeClient;
 import dev.dfonline.codeclient.config.Config;
 import dev.dfonline.codeclient.dev.InteractionManager;
+import dev.dfonline.codeclient.dev.NoClip;
 import dev.dfonline.codeclient.location.Dev;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.SlotActionType;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerInteractionManager.class)
@@ -29,6 +30,12 @@ public class MClientPlayerInteractionManager {
     @Inject(method = "interactBlock", at = @At("HEAD") , cancellable = true)
     private void onBlockInteract(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
         if(CodeClient.location instanceof Dev plot && plot.isInCodeSpace(hitResult.getBlockPos().getX(), hitResult.getPos().getZ()) && InteractionManager.onBlockInteract(player, hand, hitResult)) cir.setReturnValue(ActionResult.FAIL);
+    }
+    @Redirect(method = "interactItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V"))
+    public void interactItemMovement(ClientPlayNetworkHandler instance, Packet<?> packet) {
+        if(!NoClip.ignoresWalls()) {
+            instance.sendPacket(packet);
+        }
     }
 
     @Inject(method = "clickSlot", at = @At("HEAD"), cancellable = true)
