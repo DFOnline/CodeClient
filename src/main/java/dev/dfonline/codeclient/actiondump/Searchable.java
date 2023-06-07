1a1d96a3ac07949d7803201d7555685fdc5fa6c0
GeorgeRNG <grogrng@gmail.com>
2023-06-07 18:29:16 +0100

    Make a static searchable and fix other items tab

--- src/main/java/dev/dfonline/codeclient/actiondump/Searchable.java
+++ src/main/java/dev/dfonline/codeclient/actiondump/Searchable.java
@@ -2,9 +2,30 @@ package dev.dfonline.codeclient.actiondump;
 
 import net.minecraft.item.ItemStack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface Searchable {
     List<String> getTerms();
     ItemStack getItem();
+
+    class StaticSearchable implements Searchable {
+        private final List<String> terms;
+        private final ItemStack item;
+
+        public StaticSearchable(ItemStack item) {
+            this.terms = new ArrayList<>();
+            this.item = item;
+        }
+
+        @Override
+        public List<String> getTerms() {
+            return terms;
+        }
+
+        @Override
+        public ItemStack getItem() {
+            return item;
+        }
+    }
 }
