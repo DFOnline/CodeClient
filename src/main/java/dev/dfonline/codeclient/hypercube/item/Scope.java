5f446df6b38b566f9b9a4503af23a459622a924a
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2023-08-19 10:55:49 +0300

    Fix #53: Add line scope & use TextColor
    Since line scope is hex color and not default mc color I had to change  it to use TextColor instead

--- src/main/java/dev/dfonline/codeclient/hypercube/item/Scope.java
+++ src/main/java/dev/dfonline/codeclient/hypercube/item/Scope.java
@@ -1,17 +1,20 @@
 package dev.dfonline.codeclient.hypercube.item;
 
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.util.Colors;
 import net.minecraft.util.Formatting;
 
 public enum Scope {
-    unsaved(Formatting.GRAY, "GAME", "G"),
-    local(Formatting.GREEN, "LOCAL", "L"),
-    saved(Formatting.YELLOW, "SAVE", "S");
+    unsaved(TextColor.fromFormatting(Formatting.GRAY), "GAME", "G"),
+    local(TextColor.fromFormatting(Formatting.GREEN), "LOCAL", "L"),
+    saved(TextColor.fromFormatting(Formatting.YELLOW), "SAVE", "S"),
+    line(TextColor.fromRgb(5614335), "LINE", "L");
 
-    public final Formatting color;
+    public final TextColor color;
     public final String longName;
     public final String shortName;
-    Scope(Formatting color, String longName, String shortName) {
+    Scope(TextColor color, String longName, String shortName) {
         this.color = color;
         this.longName = longName;
         this.shortName = shortName;
