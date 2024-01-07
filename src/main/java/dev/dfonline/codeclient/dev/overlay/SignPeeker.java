16b4a1071165fe875d1f4140b41f3017c12e4328
GeorgeRNG <81434111+GeorgeRNG@users.noreply.github.com>
2024-01-07 15:44:50 +0000

    fix sign peeker working on buildspace signs

--- src/main/java/dev/dfonline/codeclient/dev/overlay/SignPeeker.java
+++ src/main/java/dev/dfonline/codeclient/dev/overlay/SignPeeker.java
@@ -15,21 +15,23 @@ import java.util.List;
 
 public class SignPeeker {
     public static List<Text> getOverlayText() {
-        if (!Config.getConfig().SignPeeker || !(CodeClient.location instanceof Dev)) return null;
-        if(CodeClient.MC.crosshairTarget instanceof BlockHitResult block) {
-            BlockPos pos = InteractionManager.isBlockBreakable(block.getBlockPos());
-            if(pos == null || CodeClient.MC.world == null) return null;
-            if(CodeClient.MC.world.getBlockEntity(pos.west()) instanceof SignBlockEntity sign) {
-                SignText text = sign.getFrontText();
-                if(CodeClient.MC.player.getX() > pos.getX()) {
-                    return List.of(
-                            Text.empty().formatted(Formatting.GOLD).append(text.getMessage(0,false)),
-                            text.getMessage(1,false),
-                            Text.empty().formatted(Formatting.AQUA).append(text.getMessage(2,false)),
-                            Text.empty().formatted(Formatting.RED).append(text.getMessage(3,false)));
+        if (!Config.getConfig().SignPeeker) return null;
+        if(CodeClient.location instanceof Dev dev) {
+            if (CodeClient.MC.crosshairTarget instanceof BlockHitResult block) {
+                BlockPos pos = InteractionManager.isBlockBreakable(block.getBlockPos());
+                if (pos == null || (!dev.isInDev(pos)) || CodeClient.MC.world == null) return null;
+                if (CodeClient.MC.world.getBlockEntity(pos.west()) instanceof SignBlockEntity sign) {
+                    SignText text = sign.getFrontText();
+                    if (CodeClient.MC.player.getX() > pos.getX()) {
+                        return List.of(
+                                Text.empty().formatted(Formatting.GOLD).append(text.getMessage(0, false)),
+                                text.getMessage(1, false),
+                                Text.empty().formatted(Formatting.AQUA).append(text.getMessage(2, false)),
+                                Text.empty().formatted(Formatting.RED).append(text.getMessage(3, false)));
+                    }
+                    Text name = text.getMessage(1, false);
+                    if (CodeClient.MC.textRenderer.getWidth(name) > 90) return List.of(name);
                 }
-                Text name = text.getMessage(1,false);
-                if(CodeClient.MC.textRenderer.getWidth(name) > 90) return List.of(name);
             }
         }
         return null;
