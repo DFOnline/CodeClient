997bf05be4eafc1048d7a96b4a6702ec0645040b
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2024-10-05 19:58:07 +0300

    fix: only attempt to hide scope messages in dev mode

--- src/main/java/dev/dfonline/codeclient/dev/MessageHiding.java
+++ src/main/java/dev/dfonline/codeclient/dev/MessageHiding.java
@@ -1,7 +1,9 @@
 package dev.dfonline.codeclient.dev;
 
+import dev.dfonline.codeclient.CodeClient;
 import dev.dfonline.codeclient.Feature;
 import dev.dfonline.codeclient.config.Config;
+import dev.dfonline.codeclient.location.Dev;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.text.Text;
@@ -16,9 +18,12 @@ public class MessageHiding extends Feature {
             Text content = message.content();
             String string = content.getString();
 
-            if (Config.getConfig().HideScopeChangeMessages) {
-                Pattern pattern = Pattern.compile("^Scope set to (GAME|SAVE|LOCAL|LINE) \\((\\w ?)+\\)\\.$");
-                return pattern.matcher(string).matches();
+            // Nested for future dev-only features.
+            if (CodeClient.location instanceof Dev) {
+                if (Config.getConfig().HideScopeChangeMessages) {
+                    Pattern pattern = Pattern.compile("^Scope set to (GAME|SAVE|LOCAL|LINE) \\((\\w ?)+\\)\\.$");
+                    return pattern.matcher(string).matches();
+                }
             }
         }
 
