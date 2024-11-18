ee1bf85e1b2348777a8de9c2361cbd9abc589618
sylviameows <66442608+sylviameows@users.noreply.github.com>
2024-11-17 23:48:17 -0600

    fix code palette gui

--- src/main/java/dev/dfonline/codeclient/dev/menu/devinventory/DevInventoryScreen.java
+++ src/main/java/dev/dfonline/codeclient/dev/menu/devinventory/DevInventoryScreen.java
@@ -16,16 +16,18 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryListener;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.ClickEvent;
@@ -45,10 +47,10 @@ import java.util.Objects;
 import static dev.dfonline.codeclient.dev.menu.devinventory.DevInventoryGroup.*;
 
 @Environment(EnvType.CLIENT)
-public class DevInventoryScreen extends CreativeInventoryScreen {
+public class DevInventoryScreen extends HandledScreen<CreativeInventoryScreen.CreativeScreenHandler> {
     static final SimpleInventory Inventory = new SimpleInventory(45);
     private static final Identifier TEXTURE = CodeClient.getId("textures/gui/container/dev_inventory/tabs.png");
-    private static final String TAB_TEXTURE_PREFIX = "textures/gui/container/creative_inventory/tab_";
+//    private static final String TAB_TEXTURE_PREFIX = "container/creative_inventory/tab_";
     private static final Text DELETE_ITEM_SLOT_TEXT = Text.translatable("inventory.binSlot");
     private static int selectedTab;
     private double scrollPosition;
@@ -64,7 +66,10 @@ public class DevInventoryScreen extends CreativeInventoryScreen {
     private boolean scrolling = false;
 
     public DevInventoryScreen(ClientPlayerEntity player) {
-        super(player, FeatureSet.empty(), CodeClient.MC.options.getOperatorItemsTab().getValue());
+        super(new CreativeInventoryScreen.CreativeScreenHandler(player), player.getInventory(), ScreenTexts.EMPTY);
+
+
+//        super(player, FeatureSet.empty(), CodeClient.MC.options.getOperatorItemsTab().getValue());
         player.currentScreenHandler = this.handler;
         this.backgroundHeight = 136;
         this.backgroundWidth = 195;
@@ -377,7 +382,7 @@ public class DevInventoryScreen extends CreativeInventoryScreen {
         String texture = "item_search";
         if (!itemGroup.hasSearchBar()) texture = "items";
         if (itemGroup == INVENTORY) texture = "inventory";
-        context.drawGuiTexture(RenderLayer::getGuiTextured, Identifier.ofVanilla(TAB_TEXTURE_PREFIX + texture + ".png"), this.x, this.y, this.backgroundWidth, this.backgroundHeight);
+        context.drawTexture(RenderLayer::getGuiTextured, ItemGroup.getTabTextureId(texture), this.x, this.y, 0.0f, 0.0f, this.backgroundWidth, this.backgroundHeight, 256, 256);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
         this.renderTabIcon(context, itemGroup);
@@ -387,9 +392,9 @@ public class DevInventoryScreen extends CreativeInventoryScreen {
             int scrollbarX = this.x + 175;
             int scrollbarY = this.y + 18;
             if (scrollHeight == 0)
-                context.drawGuiTexture(RenderLayer::getGuiTextured, TEXTURE, scrollbarX, scrollbarY, 244, 0, 256, 256, 12, 15);
+                context.drawTexture(RenderLayer::getGuiTextured, TEXTURE, scrollbarX, scrollbarY, 244, 0, 12, 15, 256, 256);
             else
-                context.drawGuiTexture(RenderLayer::getGuiTextured, TEXTURE, scrollbarX, scrollbarY + (95 * (int) (this.scrollPosition * 9) / (scrollHeight)), 256, 256, 232, 0, 12, 15);
+                context.drawTexture(RenderLayer::getGuiTextured, TEXTURE, scrollbarX, scrollbarY + (95 * (int) (this.scrollPosition * 9) / (scrollHeight)), 232, 0, 12, 15, 256, 256);
         } else {
             if (this.client != null && this.client.player != null)
                 InventoryScreen.drawEntity(context, this.x + 73, this.y + 6, this.x + 105, this.y + 49, 20, 0.0625F, (float) mouseX, (float) mouseY, this.client.player);
@@ -416,7 +421,7 @@ public class DevInventoryScreen extends CreativeInventoryScreen {
         if (isSelected) mapY += 32;
         if (!isTopRow) mapY += 64;
 
-        context.drawGuiTexture(RenderLayer::getGuiTextured, TEXTURE, originX, originY, mapX, mapY, 256, 256, TAB_WIDTH, 32);
+        context.drawTexture(RenderLayer::getGuiTextured, TEXTURE, originX, originY, mapX, mapY, TAB_WIDTH, 32, 256, 256);
 //        this.itemRenderer.zOffset = 100.0F;
         originX += 6;
         originY += 8 + (isTopRow ? 2 : -2);
