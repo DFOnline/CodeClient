a0bcb0ad1f124213141980adbde9a5bb74cea11c
Excuze <62854354+Excuze@users.noreply.github.com>
2025-03-10 12:27:37 -0400

    Pick function / process names from their signs
    Using the Pick Block action on certain codeblock signs (function, process, call function, and start process) will give you the name of the relevant function/process as a string varitem instead of giving you a useless oak sign

--- src/main/java/dev/dfonline/codeclient/mixin/world/block/MBlock.java
+++ src/main/java/dev/dfonline/codeclient/mixin/world/block/MBlock.java
@@ -2,6 +2,7 @@ package dev.dfonline.codeclient.mixin.world.block;
 
 import dev.dfonline.codeclient.CodeClient;
 import dev.dfonline.codeclient.config.Config;
+import dev.dfonline.codeclient.data.DFItem;
 import dev.dfonline.codeclient.dev.InteractionManager;
 import dev.dfonline.codeclient.hypercube.actiondump.ActionDump;
 import dev.dfonline.codeclient.location.Dev;
@@ -13,17 +14,36 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.item.Items;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 @Mixin(Block.class)
 public class MBlock {
     @Inject(method = "getPickStack", at = @At("HEAD"), cancellable = true)
     private void getPickStack(WorldView world, BlockPos pos, BlockState state, CallbackInfoReturnable<ItemStack> cir) {
+
+        if (Config.getConfig().PickFunctionNames) {
+            if (CodeClient.location instanceof Dev) {
+                ItemStack copiedNameString = tryPickFunctionName(pos);
+
+                // if that was a sign, function/process related one, and we copied its name successfully
+                if (copiedNameString != null) {
+                    cir.setReturnValue(copiedNameString);
+                    return;
+                }
+            }
+        }
+
         if (!Config.getConfig().PickAction) return;
+
         if (CodeClient.location instanceof Dev dev) {
             if (!dev.isInDev(pos)) return;
             var breakable = InteractionManager.isBlockBreakable(pos);
@@ -41,5 +61,48 @@ public class MBlock {
                 } catch (Exception ignored) {
                 }
         }
+    }
+
+    @Unique
+    private static final Pattern NAME_PICKABLE_CATEGORY_REGEX =
+            Pattern.compile("^([CALSTR]{4,5} )?((FUNCTION)|(PROCESS))$");
+
+    /**
+     * Will have changed the return value to an appropriate string if it returns true
+     * @param pos Position of the block to try and copy name from
+     * @return Null if a name can't be copied from that position, else the ItemStack of the appropriate DF string varitem
+     */
+    @Unique
+    private ItemStack tryPickFunctionName(BlockPos pos) {
+        if (CodeClient.MC.world.getBlockEntity(pos) instanceof SignBlockEntity signBlock) {
+            try {
+                SignText text = signBlock.getFrontText();
+
+                String firstLine = text.getMessage(0, false).getString();
+
+                // needs to be a CALL FUNCTION, START PROCESS, FUNCTION, or PROCESS
+                if (!NAME_PICKABLE_CATEGORY_REGEX.matcher(firstLine).matches())
+                    return null;
+
+                DFItem string = new DFItem(new ItemStack(Items.STRING, 1));
+
+                String funcName = text.getMessage(1, false).getString();
+
+                string.setName(
+                        // stop it from being italicized like anvil renaming
+                        Text.literal(funcName).setStyle(Style.EMPTY.withItalic(false))
+                    );
+
+                // there might be a better way to do this
+                string.getItemData().setHypercubeStringValue(
+                        "varitem", "{\"id\":\"txt\",\"data\":{\"name\":\"%s\"}}".formatted(funcName)
+                    );
+
+                // this was a pickable sign and we've done the picking
+                return string.getItemStack();
+            } catch (Exception ignored) {
+            }
+        }
+        return null;
     }
 }
