a1f3114f78d227faf706b0d149211efeb18207ac
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-03-12 15:12:00 +0200

    style: optimize imports

--- src/main/java/dev/dfonline/codeclient/mixin/render/MSignRenderer.java
+++ src/main/java/dev/dfonline/codeclient/mixin/render/MSignRenderer.java
@@ -1,24 +1,11 @@
 package dev.dfonline.codeclient.mixin.render;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import dev.dfonline.codeclient.CodeClient;
 import dev.dfonline.codeclient.dev.SignOutlineRenderer;
 import net.minecraft.block.entity.SignBlockEntity;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.*;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
-import net.minecraft.client.render.model.BakedModel;
-import net.minecraft.client.render.model.BakedModelManager;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.shape.SimpleVoxelShape;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
