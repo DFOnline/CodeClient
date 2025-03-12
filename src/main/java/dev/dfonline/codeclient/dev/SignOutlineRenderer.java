b14db5a7f55c529204300db635666344e6e2fdb0
Reason <28310208+Reasonlesss@users.noreply.github.com>
2025-03-12 22:00:44 +0000

    Allow seeing code search through walls

--- src/main/java/dev/dfonline/codeclient/dev/SignOutlineRenderer.java
+++ src/main/java/dev/dfonline/codeclient/dev/SignOutlineRenderer.java
@@ -6,23 +6,46 @@ import dev.dfonline.codeclient.config.Config;
 import dev.dfonline.codeclient.location.Dev;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.render.VertexRendering;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 
+import java.util.OptionalDouble;
+
 public class SignOutlineRenderer extends Feature {
 
     public static BlockPos highlightedSign = null;
 
-    public static void render(SignBlockEntity sign, MatrixStack matrices, VertexConsumerProvider vertexConsumers) {
+    private static final RenderLayer.MultiPhase ESP_LINE = RenderLayer.of(
+            "codeclient:esp_lines",
+            VertexFormats.LINES,
+            VertexFormat.DrawMode.LINES,
+            1536,
+            RenderLayer.MultiPhaseParameters.builder()
+                    .lineWidth(new RenderPhase.LineWidth(OptionalDouble.empty()))
+                    .target(RenderLayer.ITEM_ENTITY_TARGET)
+                    .writeMaskState(RenderLayer.COLOR_MASK)
+                    .depthTest(RenderLayer.ALWAYS_DEPTH_TEST)
+                    .cull(RenderLayer.DISABLE_CULLING)
+                    .build(false)
+    );
+
+    public static void render(SignBlockEntity sign, MatrixStack matrices, VertexConsumerProvider.Immediate immediate) {
         if(CodeClient.location instanceof Dev) {
             if (highlightedSign == null || !highlightedSign.equals(sign.getPos())) return;
             matrices.push();
 
             VoxelShape signShape = CodeClient.MC.world.getBlockState(sign.getPos()).getOutlineShape(CodeClient.MC.world, sign.getPos());
-            VertexRendering.drawOutline(matrices, vertexConsumers.getBuffer(RenderLayer.getLines()), signShape, 0, 0, 0, Config.getConfig().SignHighlightColor);
+
+            VertexConsumer consumer = immediate.getBuffer(ESP_LINE);
+            VertexRendering.drawOutline(matrices, consumer, signShape, 0, 0, 0, Config.getConfig().SignHighlightColor);
+            immediate.draw(ESP_LINE);
 
             matrices.pop();
         }
--- src/main/java/dev/dfonline/codeclient/mixin/render/MSignRenderer.java
+++ src/main/java/dev/dfonline/codeclient/mixin/render/MSignRenderer.java
@@ -15,7 +15,9 @@ public class MSignRenderer {
 
     @Inject(method = "render(Lnet/minecraft/block/entity/SignBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V", at = @At("RETURN"))
     private void render(SignBlockEntity sign, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, CallbackInfo ci) {
-        SignOutlineRenderer.render(sign, matrices, vertexConsumers);
+        if (vertexConsumers instanceof VertexConsumerProvider.Immediate immediate) {
+            SignOutlineRenderer.render(sign, matrices, immediate);
+        }
     }
 
 }
