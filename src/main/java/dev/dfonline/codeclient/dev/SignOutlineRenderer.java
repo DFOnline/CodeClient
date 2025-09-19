ac422da06dc5dd4e2f140cb021d236ac4249c4ad
GeorgeRNG <81434111+GeorgeRNG@users.noreply.github.com>
2025-09-19 14:28:57 +0100

    probably completely break SignOutlineRenderer.java for the purposes of compiling

--- src/main/java/dev/dfonline/codeclient/dev/SignOutlineRenderer.java
+++ src/main/java/dev/dfonline/codeclient/dev/SignOutlineRenderer.java
@@ -5,37 +5,42 @@ import dev.dfonline.codeclient.Feature;
 import dev.dfonline.codeclient.config.Config;
 import dev.dfonline.codeclient.location.Dev;
 import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.RenderPhase;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.render.VertexRendering;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 
-import java.util.OptionalDouble;
-
 public class SignOutlineRenderer extends Feature {
 
     public static BlockPos highlightedSign = null;
 
+//    private static final RenderLayer.MultiPhase SIGN_OUTLINE = RenderLayer.of(
+//            "codeclient:sign_outline",
+//            VertexFormats.POSITION_COLOR,
+//            VertexFormat.DrawMode.TRIANGLE_STRIP,
+//            1536,
+//            RenderLayer.MultiPhaseParameters.builder()
+//                    .program(RenderLayer.POSITION_COLOR_PROGRAM)
+//                    .lineWidth(new RenderPhase.LineWidth(OptionalDouble.empty()))
+//                    .layering(RenderLayer.VIEW_OFFSET_Z_LAYERING)
+//                    .(RenderLayer.TRANSLUCENT_TRANSPARENCY)
+//                    .target(RenderLayer.OUTLINE_TARGET)
+//                    .writeMaskState(RenderLayer.ALL_MASK)
+//                    .cull(RenderLayer.DISABLE_CULLING)
+//                    .build(true)
+//    );
+
     private static final RenderLayer.MultiPhase SIGN_OUTLINE = RenderLayer.of(
             "codeclient:sign_outline",
-            VertexFormats.POSITION_COLOR,
-            VertexFormat.DrawMode.TRIANGLE_STRIP,
             1536,
-            RenderLayer.MultiPhaseParameters.builder()
-                    .program(RenderLayer.POSITION_COLOR_PROGRAM)
-                    .lineWidth(new RenderPhase.LineWidth(OptionalDouble.empty()))
-                    .layering(RenderLayer.VIEW_OFFSET_Z_LAYERING)
-                    .transparency(RenderLayer.TRANSLUCENT_TRANSPARENCY)
-                    .target(RenderLayer.OUTLINE_TARGET)
-                    .writeMaskState(RenderLayer.ALL_MASK)
-                    .cull(RenderLayer.DISABLE_CULLING)
-                    .build(true)
+            false,
+            true,
+            RenderPipelines.ENTITY_OUTLINE_BLIT, // FIXME
+            RenderLayer.MultiPhaseParameters.builder().build(RenderLayer.OutlineMode.IS_OUTLINE)
     );
 
     public static void render(SignBlockEntity sign, MatrixStack matrices, VertexConsumerProvider.Immediate immediate) {
