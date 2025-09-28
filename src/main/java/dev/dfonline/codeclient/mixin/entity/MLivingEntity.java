f097d6ac4a738e45dae1c1afe99e849cb2ad6b05
GeorgeRNG <81434111+GeorgeRNG@users.noreply.github.com>
2025-09-28 18:08:27 +0100

    fix going to the moon when using teleportup

--- src/main/java/dev/dfonline/codeclient/mixin/entity/MLivingEntity.java
+++ src/main/java/dev/dfonline/codeclient/mixin/entity/MLivingEntity.java
@@ -3,9 +3,11 @@ package dev.dfonline.codeclient.mixin.entity;
 import dev.dfonline.codeclient.CodeClient;
 import dev.dfonline.codeclient.dev.Navigation;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
@@ -13,5 +15,12 @@ public class MLivingEntity {
     @Inject(method = "getJumpVelocity", at = @At("RETURN"), cancellable = true)
     private void jumpVelocity(CallbackInfoReturnable<Float> cir) {
         CodeClient.getFeature(Navigation.class).map(Navigation::jumpHeight).ifPresent(cir::setReturnValue);
+    }
+
+    @Inject(method = "jump", at = @At("HEAD"), cancellable = true)
+    private void onJump(CallbackInfo ci) {
+        if (CodeClient.getFeature(Navigation.class).map(Navigation::onJump).orElse(false)) {
+            ci.cancel();
+        }
     }
 }
--- src/main/java/dev/dfonline/codeclient/mixin/entity/player/MPlayerEntity.java
+++ src/main/java/dev/dfonline/codeclient/mixin/entity/player/MPlayerEntity.java
@@ -30,13 +30,6 @@ public abstract class MPlayerEntity extends LivingEntity {
         }
     }
 
-    @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
-    private void jump(Vec3d movementInput, CallbackInfo ci) {
-        if (this.jumping && CodeClient.getFeature(Navigation.class).map(Navigation::onJump).orElse(false)) {
-            ci.cancel();
-        }
-    }
-
     @Inject(method = "canChangeIntoPose", at = @At("HEAD"), cancellable = true)
     private void canChangeIntoPose(EntityPose pose, CallbackInfoReturnable<Boolean> cir) {
         if (CodeClient.getFeature(NoClip.class).map(NoClip::isIgnoringWalls).orElse(false)) cir.setReturnValue(true);
